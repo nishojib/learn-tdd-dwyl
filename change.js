4a898a8cf2d48ec7a858399ef0fb7df53e5967ef
Nur Islam <nishojib@gmail.com>
2022-05-09 20:53:36 -0400

    added jsdoc

--- change.js
+++ change.js
@@ -7,6 +7,15 @@ if (typeof module !== "undefined" && module.exports) {
 
 const COINS = [200, 100, 50, 20, 10, 5, 2, 1];
 
+/**
+ * getChange accepts two parameters (totalPayable and cashPaid) and calculates
+ * the change in "coins" that needs to be returned.
+ * @param {number} totalPayable the integer amount (in pennies) to be paid
+ * @param {number} cashPaid the integer amount (in pennies) the person paid
+ * @returns {array} list of coins we need to dispense to the person as change
+ * @example
+ * getChange(215, 300); // [50, 20, 10, 5]
+ */
 function getChange(totalPayable, cashPaid) {
   "use strict";
 
